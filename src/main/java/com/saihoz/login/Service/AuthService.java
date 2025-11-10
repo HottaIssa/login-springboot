3ea821ab91c332dca59c9f2288be590566df7ee3
HottaIssa <ostosissa@gmail.com>
2025-11-10 15:48:50 -0500

    fix: arreglar la codificación del password guardado

--- src/main/java/com/saihoz/login/Service/AuthService.java
+++ src/main/java/com/saihoz/login/Service/AuthService.java
@@ -1,5 +1,9 @@
 package com.saihoz.login.Service;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.saihoz.login.Jwt.JwtService;
@@ -18,15 +22,22 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     public AuthResponse login(LoginRequest request) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+            .token(token)
+            .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
         User user = User.builder()
                             .username(request.getUsername())
-                            .password(request.getPassword())
+                            .password(passwordEncoder.encode(request.getPassword()))
                             .firstname(request.getFirstname())
                             .lastname(request.getLastname())
                             .email(request.getEmail())
